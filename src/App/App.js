f6e0ce1028efb6ec078ac319f250b0235258e067
Hananiyad <alfrhnan@gmail.com>
2023-08-15 13:29:14 +0300

    commit

---
+++ src/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import layout  from '../pages/layout'
+import ResponsiveAppBar from '../Components/NavBar'
+
+function App() {
+    return(
+        <ResponsiveAppBar/> 
+
+        );
+}
+
+export default App;
